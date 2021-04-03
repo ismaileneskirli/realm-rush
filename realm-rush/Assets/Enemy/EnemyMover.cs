1aadb0b90f30d258b4ac99ed8f645b9888e2726f
ismaileneskirli <69651222+ismaileneskirli@users.noreply.github.com>
2021-04-04 00:33:30 +0300

    Enemy movement
    Enemy can find path. Textmesh pro added.

---
+++ realm-rush/Assets/Enemy/EnemyMover.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+    // Attach waypoint script to the cubes(path) like tagging them.
+    [SerializeField] List<Waypoint> path = new List<Waypoint>();
+    [SerializeField] float waitTime = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(FollowPath());
+
+    }
+
+    // IEnumerator returns sth countable, creating co-routine with waiting for 1f, yield means give up control. we are stopping for 1 sec.
+    IEnumerator FollowPath(){
+        foreach ( Waypoint waypoint in path){
+            //Debug.Log(waypoint.name);
+            transform.position = waypoint.transform.position;
+            yield return new WaitForSeconds(waitTime);
+        }
+    }
+}
---
+++ realm-rush/Assets/Tiles/CoordinateLabeler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+[ExecuteAlways]
+public class CoordinateLabeler : MonoBehaviour
+{
+
+    TextMeshPro label;
+    Vector2Int coordinates = new Vector2Int();
+
+    // very first step of game ( before start )
+    void Awake()
+    {
+        label = GetComponent<TextMeshPro>();
+        DisplayCoordinates();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //Script will only execute in edit mode.
+       if(!Application.isPlaying){
+           DisplayCoordinates();
+           UpdateObjectName();
+       }
+    }
+
+    void DisplayCoordinates(){
+        // Divided by grid that we set it as 10.
+        coordinates.x =  Mathf.RoundToInt(transform.parent.position.x / UnityEditor.EditorSnapSettings.move.x);
+        coordinates.y =  Mathf.RoundToInt(transform.parent.position.z / UnityEditor.EditorSnapSettings.move.z);
+        label.text = coordinates.x + ":" + coordinates.y;
+    }
+
+    void UpdateObjectName(){
+        transform.parent.name = coordinates.ToString();
+    }
+}
